a15345212ef9b9f65669fa0c90c55374e5cf3fe5
jsso16 <wjsthwls24@daum.net>
2021-06-01 17:19:15 +0900

    Add Save Button & Validation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import './App.css';
-import {TextField, Typography} from '@material-ui/core';
+import {TextField, Typography, Button, List, ListItem, ListItemText} from '@material-ui/core';
 import {KeyboardDatePicker, KeyboardTimePicker} from '@material-ui/pickers';
+import SaveIcon from '@material-ui/icons/Save';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      todoList: [],
       title: "",
       content: "",
       startDate: null,
       startTime: null,
+      endDate: null,
+      endTime: null
+    }
+  }
+
+  checkValidate() {
+    const {
+      title, content, startDate, startTime, endDate, endTime
+    } = this.state;
+    if (!title || !content || !startDate || !startTime || !endDate || !endTime) {
+      return false;
+    }
+    return true;
+  }
+
+  saveTodoList() {
+    if (this.checkValidate()) {
+      const {todoList, title, content, startDate, startTime, endDate, endTime} = this.state;
+      todoList.push({title: title.trim(), content: content.trim(), startDate, startTime, endDate, endTime});
+      this.setState({ 
+        todoList,
+        // title: "",
+        // content: "",
+        // startDate: null,
+        // startTime: null,
+        // endDate: null,
+        // endTime: null
+      });
+    } else {
+      alert("입력값을 확인해 주세요.");
     }
   }
 
@@ -19,27 +51,79 @@ class App extends React.Component {
       <div className="App">
         <div className="header">Todo List</div>
         <div className="input-area">
-          <TextField label="제목" size="normal" margin="normal" fullWidth required />
-          <TextField label="상세 내용" size="normal" margin="normal" fullWidth multiline />
+          <TextField label="제목" size="normal" margin="normal" fullWidth required 
+            value={this.state.title}
+            onChange={(e)=>this.setState({title:e.target.value})}
+          />
+          <TextField label="상세 내용" size="normal" margin="normal" fullWidth multiline 
+            value={this.state.content}
+            onChange={(e)=>this.setState({content:e.target.value})}
+          />
           <KeyboardDatePicker
             disableToolbar variant="inline"
             format="yyyy/MM/DD"
             margin="normal"
             label="시작 예정일"
-            onChange={(value)=>console.log(value)} 
+            value={this.state.startDate}
+            onChange={(value)=>this.setState({startDate:value})} 
             style = {{width: '50%'}} 
-            KeyboardButtonProps={{'aria-label': 'change date'}}
+            KeyboardButtonProps={{'aria-label':'change date'}}
           />
           <KeyboardTimePicker
             margin="normal"
             label="시작 시간"
             variant="inline"
-            onChange={(value)=>console.log(value)} 
+            value={this.state.startTime}
+            onChange={(value)=>this.setState({startTime:value})} 
             style = {{width: '50%'}} 
-            KeyboardButtonProps={{'aria-label': 'change time'}}
+            KeyboardButtonProps={{'aria-label':'change time'}}
           />
+          <KeyboardDatePicker
+            disableToolbar
+            variant="inline"
+            format="yyyy/MM/DD"
+            margin="normal"
+            label="종료 예정일"
+            value={this.state.endDate}
+            onChange={(value)=> this.setState({endDate:value})} 
+            style = {{width: '50%'}}     
+            KeyboardButtonProps={{'aria-label':'change date'}}
+          />
+          <KeyboardTimePicker
+            margin="normal"
+            label="종료 시간"
+            variant="inline"
+            value={this.state.endTime}
+            onChange={(value)=> this.setState({endTime:value})} 
+            style = {{width: '50%'}}   
+            KeyboardButtonProps={{'aria-label':'change time'}}
+          />
+          <Button
+            variant="outlined"
+            startIcon={<SaveIcon />}
+            style={{float:'right'}}
+            onClick={()=>this.saveTodoList()}
+          >
+            Save
+          </Button>
         </div>
-        <div className="list-area">리스트 영역</div>
+        <div className="list-area">
+        <List>
+            {this.state.todoList.map((todoItem, idx) => {
+              const {
+                title, content, startDate, startTime, endDate, endTime
+              } = todoItem;
+              return (
+                <ListItem key={idx} role={undefined} dense button>
+                  <ListItemText
+                    primary={title}
+                    secondary={startDate?.format('yyyy-MM-DD')+' '+startTime?.format('HH:MM')+' ~ '+endDate?.format('yyyy-MM-DD')+' '+endTime?.format('HH:MM')}
+                  />
+                </ListItem>
+              )
+            })}
+          </List>
+        </div>
         <div>
           <Typography varient="body2" color="textSecondary" align="center">
             {'Copyright © 전소진 ' + new Date().getFullYear() + '.'}
